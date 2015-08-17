140d428f39915abcc4fb16417b5c3c63afa248bd
Chris Conley <chrisc93@vt.edu>
2015-08-17 15:52:03 -0400

    Initial commit

---
+++ wp-content/themes/bootstrap/scripts/ayah_config.php
@@ -0,0 +1,13 @@
+<?php
+// Edit the two lines below to use the keys for your site.
+// (Note: you can find your keys at http://portal.areyouahuman.com/dashboard)
+define( 'AYAH_PUBLISHER_KEY', 'e00aa3063e0738c188e0d406e4795fb98ec446c7');
+define( 'AYAH_SCORING_KEY', '87941111e9091096bf56e9a60ecaeefb76bc9b15');
+
+
+// Set defaults for values needed by the ayah.php file.
+// (Note: you do not need to change these.)
+define( 'AYAH_WEB_SERVICE_HOST', 'ws.areyouahuman.com');
+define( 'AYAH_TIMEOUT', 0);
+define( 'AYAH_DEBUG_MODE', FALSE);
+define( 'AYAH_USE_CURL', TRUE);
