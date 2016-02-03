1dee73d9621d249f1d91db7461f7c14a7e0b86e5
Chris Conley <chrisc93@vt.edu>
2016-02-03 16:05:00 -0500

    Tweaks to header

--- wp-content/themes/totomo/header.php
+++ wp-content/themes/totomo/header.php
@@ -18,7 +18,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
 <?php wp_head(); ?>
 </head>
 
@@ -28,8 +27,20 @@
         <div class="navbar navbar-inverse navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
-                    <a href="http://csconley.com" class="navbar-brand pull-right">Chris</a>
-                </div>
+                    <a href="https://blog.csconley.com" class="navbar-brand">blog.csconley.com</a>
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		    </button>
+		</div>
+		<div class="collapse navbar-collapse navHeaderCollapse">
+			<div class="navbar-right">
+				<a class="navbar-brand" href="https://csconley.com">/home</a>
+				<a class="navbar-brand" href="https://csconley.com/about.php">/about</a>
+		
+			</div>
+		</div>
             </div>
         </div>
 
